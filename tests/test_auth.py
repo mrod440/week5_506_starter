788941060e36909494a7011c34e5dc193fac4b3d
Ubuntu <ubuntu@ip-172-31-32-90.us-west-2.compute.internal>
2026-05-08 23:01:16 +0000

    Fix test assertion for customized title

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -36,7 +36,7 @@ def test_home_page_loads(client):
     """Flask-rendered home page returns 200 and has the navbar."""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Skeleton" in response.data
+    assert b"SD Dungeon" in response.data
     # Navbar is present
     assert b"My Site" in response.data
     assert b"About" in response.data
